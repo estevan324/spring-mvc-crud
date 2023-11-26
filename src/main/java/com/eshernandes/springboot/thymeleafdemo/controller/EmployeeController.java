c7344b7c16d10890040ef70b6c54bb4c4eff955a
Estevan Hernandes Silvade Oliveira <estevanhernandes1@gmail.com>
2023-11-26 15:00:22 -0300

    update employee function

--- src/main/java/com/eshernandes/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/eshernandes/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -5,10 +5,7 @@ import com.eshernandes.springboot.thymeleafdemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -35,6 +32,17 @@ public class EmployeeController {
     @GetMapping("/create")
     public String createEmployee(Model model) {
         model.addAttribute("employee", new Employee());
+
+        return "employees/employee-form";
+    }
+
+    @GetMapping("/update")
+    public String updateEmployee(
+            @RequestParam("employeeId") int employeeId, Model model) {
+
+        Employee employee = employeeService.findById(employeeId);
+
+        model.addAttribute("employee", employee);
 
         return "employees/employee-form";
     }
